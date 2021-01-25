d90dd1bd9c91d0bc48aaaeda619f0dee7e60d6da
jtam2 <jacobtamus@gmail.com>
2021-01-25 14:31:33 -0700

    put sleep

--- tutorial/api/views.py
+++ tutorial/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+import time
 class BasicAPI(APIView):
 
     def get(self, request):
@@ -9,7 +9,7 @@ class BasicAPI(APIView):
             'first_name': 'grant',
             'last_name': 'zhu'
         }
-        return Response(data, status=status.HTTP_200_Ok)
+        return Response(data, status=status.HTTP_200_OK)
 
     def post(self, request):
         req_data = request.data
@@ -17,4 +17,5 @@ class BasicAPI(APIView):
             'first_name': req_data.get('first_name'),
             'last_name': req_data.get('last_name')
         }
+        time.sleep(900)
         return Response(data, status=status.HTTP_200_OK)
